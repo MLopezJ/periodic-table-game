025692514a256f0bc794d32b08b91df6fe859299
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2020-05-08 22:36:03 -0600

    bug fixing: delete button in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,12 +12,10 @@ class Footer extends Component{
     constructor() {
         super();
         this.state = { 
-            showDedicationMessage: false,
             title : undefined,
             language : undefined
          };
 
-        this.toggleShowDedicationMessage = this.toggleShowDedicationMessage.bind(this);
         this.setTitle = this.setTitle.bind(this);
         this.toggleLanguage = this.toggleLanguage.bind(this);   
     }
@@ -53,89 +51,12 @@ class Footer extends Component{
         }
     }
 
-    toggleShowDedicationMessage() {
-        this.setState({
-            showDedicationMessage : !this.state.showDedicationMessage
-        });
-    }
-
-    dedicationMessage = () => {
-        if (this.state.showDedicationMessage){
-
-        return(
-            <div>
-                <div className={'overlay'} onClick={this.toggleShowDedicationMessage}></div>
-                <div className={'styleElementModal modal modal-page '}> 
-                    <div className={'box'}>
-                            {
-                                <div className={'box-body-information'}>
-                                Created by:
-                                <div className={'box-header information alkali_metals'}> {/*  ${this.state.chemicalElement.group} */}
-                        
-                            <div className={'element-name'}>
-                                {'Mauro'}
-                            </div>
-
-                            <div className={'element-group'}>
-                                {'López'}
-                            </div>
-
-                            <div className={'element-group'}>
-                                {'Jiménez'}
-                            </div>
-                        </div>
-                                </div>  
-                            }
-
-                        <div className={'box-body'}>
-
-                             
-                            {
-                                <div className={'box-body-information-footer information basic_metals'}>
-                                Enthusiastic team player, techno utopian and deep creative thinker.
-                                </div>  
-                            }
-                            {
-                                <div className={'box-body-information-footer information actinoids'}>
-                                Constant, dynamic and willing to take challenges.
-                                </div>  
-                            }
-
-                            {
-                                <div className={'box-body-information-footer information amphigens'}>
-                                Mail: lopezjimenezmauro05@gmail.com
-                                </div>  
-                            }
-                            {
-                                <div className={'box-body-information-footer information noble_gases'}>
-                                Github: MLopezJ
-                                </div>  
-                            }
-                            {
-                                <div className={'box-body-information-footer information transition_metals'}>
-                                Twitter: MauroLopezJ
-                                </div>  
-                            }
-                            
-                            
-                        </div>
-                    </div>
-                </div>
-        </div>
-        )
-        }
-    }
 
     render(){
         
         return(
             <div>
-                {this.dedicationMessage()}
                 <div className={'footer'}>
-                    <div id="authorDescription" onClick={this.toggleShowDedicationMessage}>
-                        <h1 className={'title'}>{'<MauroLopez/>'}</h1>
-                    </div>
-
                     <div id="settings" onClick={this.props.setShowSettings}>
                         <h1 className={'title'}>{this.state.title}</h1>
                     </div>
